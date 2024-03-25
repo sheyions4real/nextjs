15db6b51eb0809adc09e58053e2f2f8d75f0e7a3
Sheyi Omagene <sheyions4real@yahoo.co.uk>
2024-03-25 17:53:10 -0500

    Feature: Darkmode and light mode

--- movie_app/bridge_view/src/components/Header.jsx
+++ movie_app/bridge_view/src/components/Header.jsx
@@ -3,6 +3,7 @@ import Menuitem from './MenuItem'
 import {AiFillHome} from 'react-icons/ai'
 import { BsFillInfoCircleFill } from 'react-icons/bs'
 import Link from 'next/link'
+import DarkModeSwitch from './DarkModeSwitch'
 
 
 export default function Header() {
@@ -12,10 +13,14 @@ export default function Header() {
           <Menuitem title="home" address="/" Icon={AiFillHome} />
           <Menuitem title="about" address="/about" Icon={BsFillInfoCircleFill} />
         </div>
-        <Link href="/" className='flex gap-1 items-center'>
-            <span className='text-2xl font-bold bg-amber-500 py-0.5 px-2 rounded-2xl'>Bridge</span>
-            <span className='text-xl hidden font-bold sm:inline py-1.5 '>View</span>
-        </Link>
+        <div className="flex items-center gap-4">
+          <DarkModeSwitch />
+
+          <Link href="/" className='flex gap-1 items-center'>
+              <span className='text-2xl font-bold bg-amber-500 py-0.5 px-2 rounded-2xl'>Bridge</span>
+              <span className='text-xl hidden font-bold sm:inline py-1.5 '>View</span>
+          </Link>
+        </div>
     </div>
   )
 }
